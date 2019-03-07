4918afb34ee3283e5882f58172229f0ff40e55d9
Anguliachao <Anguliachao@users.noreply.github.com>
2019-03-08 00:37:53 +0800

    Create 136_Single Number.py

---
+++ 136_Single Number.py
@@ -0,0 +1,26 @@
+
+
+###Python version
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return  2*sum(set(nums)) - sum(nums)
+        
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        #return 2*sum(set(nums)) - sum(nums)
+        result = 0
+        for i in nums:
+            result^=i
+        return result        
+        
+  
+###C++ version
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for(int x=0;x<nums.size();x++)
+            result^=nums[x];
+        return result;
+    }
+};
