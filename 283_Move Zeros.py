b6ee1165437307cfd49067f98a665406a4855ecd
Anguliachao <Anguliachao@users.noreply.github.com>
2019-03-11 22:21:18 +0800

    Create 283_Move Zeros.py

---
+++ 283_Move Zeros.py
@@ -0,0 +1,30 @@
+## Python
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        cc =0
+        for i in nums:
+            if i!=0:
+                nums[cc] = i
+                cc+=1
+        for j in range(cc,len(nums)):
+            nums[j] = 0
+        
+##C++
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        for(;j<nums.size();j++){
+            nums[j] = 0;
+        }
+    }
+};
