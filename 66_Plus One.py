2a0852afb1f6f17973d66ccc8810408ab59230cb
Anguliachao <Anguliachao@users.noreply.github.com>
2019-05-23 13:03:39 +0800

    Update 66_Plus One.py

--- 66_Plus One.py
+++ 66_Plus One.py
@@ -26,6 +26,15 @@ class Solution:
                 digits[x]=0
         digits.insert(0,1)
         return digits
+#Optimized python solution2
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        cur = 1
+        for i in range(len(digits)-1,-1,-1):
+            d = digits[i]
+            cur,digits[i] = (cur+d)/10,(cur+d)%10
+        return [1]+digits if cur==1 else digits
+            
         
 ##C++
 class Solution {
